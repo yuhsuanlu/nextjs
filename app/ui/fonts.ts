7d244732d01c556be64021458bd685829a290c53
YuHsuanLu <46243445+YuHsuanLu@users.noreply.github.com>
2023-12-27 22:33:17 -0800

    1st commit

---
+++ app/ui/fonts.ts
@@ -0,0 +1,8 @@
+import { Inter, Lusitana } from 'next/font/google';
+// primary font
+export const inter = Inter({ subsets: ['latin'] });
+// secondary font
+export const lusitana = Lusitana({
+    weight: ['400', '700'],
+    subsets: ['latin'],
+});
